5bbc95a758d976005001c36d69f368dfb5febf8b
Menium <meus13@gmail.com>
2025-06-20 16:24:33 +0200

    Small changes in Cmake and testing

--- tests/testing.cc
+++ tests/testing.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
-#include "../main.cc"
+#include "../utils/mathutils.h"
 
 TEST(AdditionTest, Adding){
     EXPECT_EQ(my_math::add(2,3), 7);
+}
+TEST(AdditionTest, AddingFailed){
+    EXPECT_EQ(my_math::add(2,3), 9);
 }
