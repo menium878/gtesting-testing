5ddcd3d0e939ef4cf5085ca3eccd26c486faa63c
Menium <meus13@gmail.com>
2025-06-22 18:37:04 +0200

    Move setter for rvalue

--- include/core/animal.h
+++ include/core/animal.h
@@ -7,7 +7,8 @@ public:
     Animal(std::string name, int age);
     ~Animal() = default;
     const std::string& getName() const noexcept;
-    void setName(const std::string& name);
+    void setName(const std::string& _name);
+    void setName(std::string&& _name) noexcept;
 private:
     std::string name;
     int age;
--- src/core/animal.cc
+++ src/core/animal.cc
@@ -1,11 +1,15 @@
 #include "core/animal.h"
 
 Animal::Animal(std::string name, int age)
-:name(name),age(age){};
+:name(std::move(name)),age(age){};
 
 const std::string& Animal::getName() const noexcept {
     return name;
 }
-void Animal::setName(const std::string& name){
-    this->name = name;
+void Animal::setName(const std::string& _name){
+    name = _name;
+}
+
+void Animal::setName(std::string&& _name) noexcept{
+    name = std::move(_name);
 }
