e03df083824b0c71c5feacd1af665b0923347a92
Menium <meus13@gmail.com>
2025-06-28 22:16:04 +0200

    Added test for animal

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <memory>
 #include "../include/utils/mathutils.h"
+#include "core/animal.h"
 int main(){
     std::cout << my_math::add(2,3) <<"\n";
+    std::shared_ptr<Animal> dog = std::make_shared<Animal>("Max",7);
     return 0;
 }
--- tests/testing.cc
+++ tests/testing.cc
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
+#include <memory>
 #include "utils/mathutils.h"
+#include "core/animal.h"
 
 TEST(AdditionTest, Adding){
     EXPECT_EQ(my_math::add(2,3), 7);
 }
 TEST(AdditionTest, AddingFailed){
     EXPECT_EQ(my_math::add(2,3), 9);
+}
+TEST(AnimalTest, AnimalTest){
+    std::unique_ptr<Animal> animal = std::make_unique<Animal>("Cat",7);
+    EXPECT_EQ(animal->getName(), "Cat");
+    animal->setName("Dog");
+    EXPECT_EQ(animal->getName(), "Dog");
 }
